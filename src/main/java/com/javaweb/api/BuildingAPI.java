8e397c18796e7e276b41def20b7508be990176dd
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2024-07-05 20:13:28 +0700

    Learn rule 2, 3, 8, 4, 5, 6, HTTP Reponse Status Code, custom Exception

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,7 +1,9 @@
 package com.javaweb.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,26 +11,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.Beans.BuildingDTO;
+import com.javaweb.Beans.ErrorResponseDTO;
+
+import customexception.FieldRequiredException;
 
 @RestController
 public class BuildingAPI {
-	@GetMapping(value="/api/building/")
-	public BuildingDTO getBuilding(@RequestParam(value="name", required = false) String name,
-							@RequestParam(value="numberOfBasement", required = false) Integer numberOfBasement,
-							@RequestParam(value="ward", required = false) String ward) {
+	@PostMapping(value="/api/building/")
+	public Object getBuilding(@RequestBody BuildingDTO buildingDTO) {
 		//xu li duoi DB xong roi
-		BuildingDTO result = new BuildingDTO();
-		result.setName(name);
-		result.setNumberOfBasement(numberOfBasement);
-		result.setWard(ward);
-		return result;
+		try {
+			valiDate(buildingDTO);
+		} catch (FieldRequiredException e) {
+			ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+			errorResponseDTO.setError(e.getMessage());
+			List<String> details = new ArrayList<String>();
+			details.add("Check name or numberOfBasement again because there is null!!!");
+			errorResponseDTO.setDetail(details);
+			return errorResponseDTO;
+		}
+		return null;
 	}
 	
-	@PostMapping(value="/api/building/")
-	public BuildingDTO getBuilding2(@RequestBody BuildingDTO buildingDTO) {
-		//sau khi xu ly duoi DB
-		return buildingDTO;
+	public void valiDate(BuildingDTO buildingDTO) throws FieldRequiredException {
+		if(buildingDTO.getName() == null || buildingDTO.getName().equals("") 
+				|| buildingDTO.getNumberOfBasement() == null) {
+			throw new FieldRequiredException("Name or number of basement is null");
+		}
 	}
+	
+//	@PostMapping(value="/api/building/")
+//	public BuildingDTO getBuilding2(@RequestBody BuildingDTO buildingDTO) {
+//		//sau khi xu ly duoi DB
+//		return buildingDTO;
+//	}
 	
 	@DeleteMapping(value="/api/building/{id}/{name}") 
 	public void deleteBuilding(@PathVariable Integer id, 
