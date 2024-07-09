3bc2d7eeeff6506445495fdd60f003bd245f8395
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2024-07-09 15:33:18 +0700

    Learn rule 7, 11, fix HTTP Status Code by @ControllerAdvice

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,8 +1,5 @@
 package com.javaweb.api;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,45 +8,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.Beans.BuildingDTO;
-import com.javaweb.Beans.ErrorResponseDTO;
 
 import customexception.FieldRequiredException;
 
 @RestController
 public class BuildingAPI {
-	@PostMapping(value="/api/building/")
+	@PostMapping(value = "/api/building/")
 	public Object getBuilding(@RequestBody BuildingDTO buildingDTO) {
-		//xu li duoi DB xong roi
-		try {
-			valiDate(buildingDTO);
-		} catch (FieldRequiredException e) {
-			ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
-			errorResponseDTO.setError(e.getMessage());
-			List<String> details = new ArrayList<String>();
-			details.add("Check name or numberOfBasement again because there is null!!!");
-			errorResponseDTO.setDetail(details);
-			return errorResponseDTO;
-		}
+		// xu li duoi DB xong roi
+		valiDate(buildingDTO);
 		return null;
 	}
-	
-	public void valiDate(BuildingDTO buildingDTO) throws FieldRequiredException {
-		if(buildingDTO.getName() == null || buildingDTO.getName().equals("") 
+
+	public void valiDate(BuildingDTO buildingDTO) {
+		if (buildingDTO.getName() == null || buildingDTO.getName().equals("")
 				|| buildingDTO.getNumberOfBasement() == null) {
 			throw new FieldRequiredException("Name or number of basement is null");
 		}
 	}
-	
+
 //	@PostMapping(value="/api/building/")
 //	public BuildingDTO getBuilding2(@RequestBody BuildingDTO buildingDTO) {
 //		//sau khi xu ly duoi DB
 //		return buildingDTO;
 //	}
-	
-	@DeleteMapping(value="/api/building/{id}/{name}") 
-	public void deleteBuilding(@PathVariable Integer id, 
-								@PathVariable String name,
-								@RequestParam(value="ward", required = false) String ward) {
+
+	@DeleteMapping(value = "/api/building/{id}/{name}")
+	public void deleteBuilding(@PathVariable Integer id, @PathVariable String name,
+			@RequestParam(value = "ward", required = false) String ward) {
 		System.out.println("Xoá " + id + " và " + name + " ròi nè");
 	}
 }
---
+++ src/main/java/com/javaweb/controlleradvice/ControllerAdvidsor.java
@@ -0,0 +1,41 @@
+package com.javaweb.controlleradvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.javaweb.Beans.ErrorResponseDTO;
+
+import customexception.FieldRequiredException;
+
+@ControllerAdvice
+public class ControllerAdvidsor extends ResponseEntityExceptionHandler{
+	@ExceptionHandler(ArithmeticException.class)
+	public ResponseEntity<Object> handleArithmeticException(
+			ArithmeticException ex, WebRequest request) {
+		ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+		errorResponseDTO.setError(ex.getMessage());
+		List<String> details = new ArrayList<String>();
+		details.add("Cannot didvide by zero");
+		errorResponseDTO.setDetail(details);
+		return new ResponseEntity<Object>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(FieldRequiredException.class)
+	public ResponseEntity<Object> handleFieldRequiredException(
+			FieldRequiredException ex, WebRequest request) {
+		ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+		errorResponseDTO.setError(ex.getMessage());
+		List<String> details = new ArrayList<String>();
+		details.add("Check name or numberOfBasement again because there is null!!!");
+		errorResponseDTO.setDetail(details);
+		//nhận đc request không hợp lệ thì thường là 502
+		return new ResponseEntity<Object>(errorResponseDTO, HttpStatus.BAD_GATEWAY);
+	}
+}
